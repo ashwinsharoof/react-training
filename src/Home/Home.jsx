6d1170c73ca7e6021345b175aa712fba0457b960
Ashwinajackus <ashwin.rajaram@ajackus.com>
2022-11-24 11:01:38 +0530

    useEffect class

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,18 +1,54 @@
 import './Home.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function Home(){
-    const [data, setData] = useState("Ram")
-    const [obj, setObj] = useState({})
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [obj, setObj] = useState(0)
     const [arr, setArr] = useState([1,2,3,4])
-    function update(){
-        setData("Kumar")
-        
-    }
+
+    useEffect(() =>{
+        const fetch_data = async()=>{
+            let response = await axios.get("https://jsonplaceholder.typicode.com/todos")
+            console.log(response)
+            if(response){
+                setData(response.data)
+                setLoading(false)
+            }
+            
+        }
+        fetch_data()
+     },[])  
+
+     const todo_delete = async(id) =>{
+        let response = await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+
+     }
+
     return(
         <div className="home">
-            <h1>{data}</h1>
-            <button onClick={update}>Click</button>
+            {loading? 
+            <p>Please wait Loading</p>
+            :
+            <>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                </tr>
+
+            {data.map((data) => (
+                <tr>
+                    <td>{data.id}</td>
+                    <td>{data.title}</td>
+                    <td><button>Edit</button></td>
+                    <td><button onClick={todo_delete(data.id)}>Delete</button></td>
+                </tr>
+            ))}
+            </table>
+            </>
+            }
         </div>
     )
 }
